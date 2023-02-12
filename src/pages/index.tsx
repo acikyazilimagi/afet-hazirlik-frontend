ce80f66716341c1996beca7dd0f604504e310a6c
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-02-12 18:06:26 +0300

    feat: add favicons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,7 +8,7 @@ export default function Home({ API_URL }: any) {
   return (
     <>
       <Head>
-        <title>AfetHazırlık - Olası afetlere karşı hazır olun</title>
+        <title>Afet Hazırlık - Olası afetlere karşı hazırlıklı olun</title>
       </Head>
 
       <Landing />

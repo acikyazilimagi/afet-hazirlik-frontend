2ffc183c0e4c44c26718d634771b6f7ab24e7e40
Zafer AYAN <ozcanzaferayan@gmail.com>
2023-02-12 18:19:58 +0300

    fix: menus

--- src/components/Prepare/Page.tsx
+++ src/components/Prepare/Page.tsx
@@ -2,20 +2,16 @@ import Breadcrumb from "@/components/Prepare/Breadcrumb";
 import Header from "@/components/Prepare/Header";
 import Main from "@/components/Prepare/Main";
 import SideMenu from "@/components/Prepare/SideMenu";
-import { Layout, theme } from "antd";
+import { Layout } from "antd";
 import React from "react";
-import { JsxElement } from "typescript";
 
 const Page = ({ children }: any) => {
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
   return (
     <>
       <Layout>
-        {/* <Header /> */}
+        <Header />
         <Layout>
-          {/* <SideMenu /> */}
+          <SideMenu />
           <Layout style={{ padding: "0 24px 24px" }}>
             <Breadcrumb />
             <Main>{children}</Main>
